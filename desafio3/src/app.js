654c67571b0fedb2c75521e218f66435fd3c4949
agosmosquera <agosmsoquera23@gmail.com>
2023-07-06 22:07:43 -0300

    desafio3

--- desafio3/src/app.js
+++ desafio3/src/app.js
@@ -12,3 +12,77 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`servidor en el puerto ${PORT}`);
 });
+
+// desafio
+
+const productos = [
+  { 
+      id: 1,
+      producto : "jabon",
+      marca : "liquido",
+      precio: 30,
+  },
+  { 
+    id: 2,
+    producto : "jabon",
+    marca : "solido",
+    precio: 50,
+  },
+  { 
+      id: 3,
+      producto : "shampo",
+      marca : "liquido",
+      precio: 45,
+  },
+  { 
+      id: 4,
+      producto : "shampoo",
+      marca : "solido",
+      precio: 60,
+  },
+  { 
+      id: 5,
+      producto : "fideos",
+      marca : "luccheti",
+      precio: 21,
+  },
+  { 
+    id: 6,
+    producto : "fideos",
+    marca : "marimar",
+    precio: 23,
+},
+{ 
+  id: 7,
+  producto : "arroz",
+  marca : "luccheti",
+  precio: 20,
+},
+{ 
+  id: 8,
+  producto : "arroz",
+  marca : "miramar",
+  precio: 18,
+},
+{ 
+  id: 9,
+  producto : "polenta",
+  marca : "luccheti",
+  precio: 10,
+},
+{ 
+  id: 10,
+  producto : "jugo",
+  marca : "sancor",
+  precio: 3,
+}
+]
+
+app.get('/bienvenida', (req, res) => {
+  res.send('<h1 style="color:blue"> Hola cliente </h1>');
+});
+
+
+app.get('/productos', (req, res) => {
+  res.json(productos);
+})
